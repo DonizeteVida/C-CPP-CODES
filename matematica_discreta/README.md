2a5b5223e58a4f63b1a228970ec7cec28706a3a3
Donizete Vida <donizetevida1998@outlook.com>
2020-03-01 18:18:25 -0300

    Calculadora de matemática discreta

---
+++ matematica_discreta/README.md
@@ -0,0 +1,2 @@
+Para criar uma biblioteca: gcc -o lib.o -c lib.c
+Para fazer a link edicao: gcc main.c -o main.o lib.o
---
+++ matematica_discreta/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "types.h"
+
+int main(int argc, char const *argv[])
+{
+    printf("%d", add(5, 10));
+    return 0;
+}
