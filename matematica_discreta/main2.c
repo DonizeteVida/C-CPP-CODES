56026612258ba16077263e0a9e038da084826424
Donizete Vida <donizetevida1998@outlook.com>
2020-04-21 14:56:23 -0300

    Fix little errors

--- matematica_discreta/main2.c
+++ matematica_discreta/main2.c
@@ -10,14 +10,14 @@ int main(int argc, char const *argv[])
     printf("Digite os valores iniciais aos futuros calculos: \n");
     printf("O primeiro valor é p: ");
     scanf("%d", &p);
-    printf("Agora digite o valor de q: \n");
+    printf("Agora digite o valor de q: ");
     scanf("%d", &q);
-    system("cls");
+    system("clear");
 
     do
     {
-        printf("MENU\n1. ~ p\n2. ~ q\n3. p /\\ q\n4. p \\/\n5. \\/*\n6. p -> q\n7. p <-> q\n8. Sair.\n");
-        print("Digite um valor para a opcão: ");
+        printf("MENU\n1. ~ p\n2. ~ q\n3. p /\\ q\n4. p \\/ q\n5. p \\/* q\n6. p -> q\n7. p <-> q\n8. Sair.\n");
+        printf("Digite um valor para a opcão: ");
         scanf("%d", &option);
 
         switch (option)
@@ -39,8 +39,8 @@ int main(int argc, char const *argv[])
         case 8:
             break;
         default:
-            system("cls");
-            printf("Digite um valor válido...");
+            system("clear");
+            printf("Digite um valor válido.../n");
         }
     } while (option != 8);
     return 0;
