0fa8a0d6cb2526049b2a4ef2f99d9f44ef1cc7ff
Donizete Vida <donizetevida1998@outlook.com>
2020-04-21 14:59:09 -0300

    Early return for wrong values

--- matematica_discreta/main2.c
+++ matematica_discreta/main2.c
@@ -14,11 +14,18 @@ int main(int argc, char const *argv[])
     scanf("%d", &q);
     system("clear");
 
+    if ((p > 1 || p < 0) || (q > 1 || q < 0))
+    {
+        return 0;
+    }
+
     do
     {
         printf("MENU\n1. ~ p\n2. ~ q\n3. p /\\ q\n4. p \\/ q\n5. p \\/* q\n6. p -> q\n7. p <-> q\n8. Sair.\n");
         printf("Digite um valor para a opcão: ");
         scanf("%d", &option);
+
+        int finalResult = -1;
 
         switch (option)
         {
