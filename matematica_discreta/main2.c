7a22552c1c65c57c65f99a8783ef077b7992453f
Donizete Vida <donizetevida1998@outlook.com>
2020-04-21 14:51:18 -0300

    Switch options

--- matematica_discreta/main2.c
+++ matematica_discreta/main2.c
@@ -9,8 +9,31 @@ int main(int argc, char const *argv[])
     do
     {
         printf("MENU\n1. ~ p\n2. ~ q\n3. p /\\ q\n4. p \\/\n5. \\/*\n6. p -> q\n7. p <-> q\n8. Sair.\n");
-        print("Digite um valor: ");
+        print("Digite um valor para a opcão: ");
         scanf("%d", &option);
+
+        switch (option)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        default:
+            system("cls");
+            printf("Digite um valor válido...");
+        }
     } while (option != 8);
     return 0;
 }
